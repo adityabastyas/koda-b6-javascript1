e7471aaa3f1a8426b84e00808b67d33dbd9cc316
aditya bastyas <adityabastyasmulya@gmail.com>
2025-12-24 16:08:43 +0700

    Update circle calculations with conditional phi
    Refactor circle area and circumference calculation to use conditional for phi value.

--- task1.js
+++ task1.js
@@ -1,10 +1,24 @@
 // hitung luas dan keliling lingkaran
 
+// let r = 15;
+// const phi = 3.14;
+
+// const luasLingkaran = phi * r * r;
+// console.log("luas lingkaran adalah " + luasLingkaran);
+
+// const kelilingLingkaran = 2 * phi * r;
+// console.log("keliling lingkaran adalah " + kelilingLingkaran);
+
 let r = 15;
-const phi = 3.14;
+let phi;
+
+if (r % 7 === 0) {
+  phi = 22 / 7;
+} else {
+  phi = 3.14;
+}
 
 const luasLingkaran = phi * r * r;
-console.log("luas lingkaran adalah " + luasLingkaran);
-
 const kelilingLingkaran = 2 * phi * r;
-console.log("keliling lingkaran adalah " + kelilingLingkaran);
+console.log(luasLingkaran);
+console.log(kelilingLingkaran);
