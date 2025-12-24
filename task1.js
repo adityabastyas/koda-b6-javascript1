233d26a9fa5b5ae7139004195e2599929d22c5fa
aditya bastyas <adityabastyasmulya@gmail.com>
2025-12-24 10:48:52 +0700

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,10 @@
+// hitung luas dan keliling lingkaran
+
+const r = 15;
+const phi = 3.14;
+
+const luasLingkaran = `luas lingkaran adalah ${phi * r * r}`;
+console.log(luasLingkaran);
+
+const kelilingLingkaran = `keliling lingkaran adalah ${2 * phi * r}`;
+console.log(kelilingLingkaran);
